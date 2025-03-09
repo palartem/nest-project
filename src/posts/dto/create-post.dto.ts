92f824bec880b01dfb40ddce1aa95bdb9312bcd7
Artem Palashek <palartem@gmail.com>
2025-03-09 22:18:36 +0200

    - dto post update

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,18 +1,1 @@
-import { ApiProperty } from "@nestjs/swagger";
-export class CreatePostDto {
-    @ApiProperty ({
-        description: "Название объявления",
-    })
-    title: string
-
-    @ApiProperty ({
-        description: "Название объявления",
-    })
-    description: string
-
-    @ApiProperty ({
-        description: "ID категории",
-        minimum: 1,
-    })
-    CategoryId: number
-}
+export class CreatePostDto {}
