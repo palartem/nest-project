ce4f09fcd1f5fbf9b148c5a00426cec02c0d18e6
Artem Palashek <palartem@gmail.com>
2025-03-09 22:21:50 +0200

    - Документация swagger для постов

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePostDto {}
+import { ApiProperty } from "@nestjs/swagger";
+export class CreatePostDto {
+    @ApiProperty ({
+        description: "Название объявления",
+    })
+    title: string
+
+    @ApiProperty ({
+        description: "Название объявления",
+    })
+    description: string
+
+    @ApiProperty ({
+        description: "ID категории",
+        minimum: 1,
+    })
+    CategoryId: number
+}
